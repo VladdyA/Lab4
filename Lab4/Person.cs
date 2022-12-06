1bf84a7174fb9f2e03beaac8e5aeb9d75d31fe9b
User <User@DESKTOP-RPSJ6FD>
2022-12-06 17:41:10 +0200

    Add project files.

---
+++ Lab4/Person.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Person
+{
+    private string name;
+    private int birthYear;
+    private DateTime date = DateTime.Today;
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public int BirthYear
+    {
+        get { return birthYear; }
+    }
+
+    public Person() : this("Vlad", 2003)
+    {
+    }
+    public Person(string name, int birthYear)
+    {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    public void Age()
+    {
+        birthYear = date.Year - birthYear;
+        Console.WriteLine(birthYear);
+    }
+
+    public void Input()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine("Введіть ім'я:");
+        this.name = Console.ReadLine();
+        Console.WriteLine("Рік народження:");
+        this.birthYear = Convert.ToInt32(Console.ReadLine());
+    }
+
+    public void ChangeName(string newName)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        this.name = newName;
+        Console.WriteLine($"Ваше ім'я було змінено на: {newName}");
+    }
+
+    public void Output()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine($"Ім'я: {this.name}, Вік: {this.birthYear}");
+    }
+
+    public override string ToString()
+    {
+        return $"Ім'я: {this.name}, Вік: {this.birthYear}";
+    }
+
+    public static bool operator ==(Person p1, Person p2)
+    {
+        return (p1.name == p2.name);
+    }
+    public static bool operator !=(Person p1, Person p2)
+    {
+        return (p1.name != p2.name);
+    }
+    public void Comparison(Person p1, Person p2)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        if (p1 == p2)
+        {
+            Console.WriteLine($"Особи з однаковими іменами, їх інформація: {p1}; {p2}");
+        }
+        else
+        {
+            Console.WriteLine($"В осіб не однакові імена");
+        }
+    }
+}
---
+++ Lab4/Programm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+
+{
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Натисніть любу кнопку для того щоб побачити дані про Person");
+            Console.ReadKey();
+
+            Person p1 = new Person("Oleg", 2004);
+            Person p2 = new Person("Albert", 1995);
+            Person p3 = new Person("Max", 1997);
+            Person p4 = new Person("Andrii", 2007);
+            Person p5 = new Person("Vlad", 2003);
+            Person p6 = new Person("John", 2006);
+
+            p1.Age();
+            p2.Age();
+            p3.Age();
+            p4.Age();
+            p5.Age();
+            p6.Age();
+
+            p1.Output();
+            p2.Output();
+            p3.Output();
+            p4.Output();
+            p5.Output();
+            p6.Output();
+
+            p4.ChangeName("Stepan Radniy");
+            p4.Output();
+
+            Console.WriteLine($"Інформація про всіх осіб: {p1}; {p2}; {p3}; {p4}; {p5}; {p6}");
+
+            p1.Comparison(p1, p4);
+            p1.Comparison(p1, p6);
+
+            Console.WriteLine("Натисніть любу кнопку для того щоб закрити консоль");
+            Console.ReadKey();
+        }
+    }
+}
